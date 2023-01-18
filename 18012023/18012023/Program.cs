fe1ff37cb07b24e5fdb235e7a4d1329822186838
eaghayev <eaghayevv@gmail.com>
2023-01-18 16:50:36 +0400

    project added

---
+++ 18012023/18012023/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace _18012023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowHello();
+            string text = "Salam P132";
+            Show(text);
+
+            int cem = Sum(40, 65);
+
+            Console.WriteLine("Ad:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyad:");
+            string surname = Console.ReadLine();
+
+            var fullname = MakeFullname(name, surname);
+
+            Console.WriteLine(fullname); //Hikmet Abbasov
+
+
+            var sum = SumOfDigits(345);
+
+            Console.WriteLine(sum);
+
+            Console.WriteLine(HasA("Hikmet"));
+
+            var check = IsDigit('a');
+
+            Console.WriteLine(check);
+        }
+
+        //Verilmis charin reqem olub olmadigini tapan metod
+
+        static bool IsDigit(char symbol)
+        {
+            //if (cr >= 48 && cr <= 57)
+            //{
+            //    return true;
+            //}
+            //else return false;
+
+            char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            //string str = "0123456789";
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] == symbol)
+                    return true;
+            }
+
+            return false;
+        }
+
+        static int SumOfDigits(int num)
+        {
+            var sum = 0;
+            while (num > 0)
+            {
+                var digit = num % 10;
+                sum += digit;
+                num /= 10;
+            }
+
+            return sum;
+        }
+
+        static int Avg(int num1,int num2)
+        {
+            int count = 0;
+            int sum = 0;
+            for (int i = num1; i < num2; i++)
+            {
+                if (i % 21 == 0)
+                {
+                    sum += i;
+                    count++;
+                }
+            }
+
+            return sum / count;
+        }
+
+        static bool HasA(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void ShowHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static void Show(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static int Sum(int num1, int num2)
+        {
+            var result = num1 + num2;
+
+            return result;
+        }
+
+        static string MakeFullname(string name,string surname)
+        {
+            var fn = name +" "+ surname;
+
+            return fn;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 18012023/Lesson/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lesson
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //ShowGroupNo();
+            //ShowStudentCount();
+
+            ShowGroupInfo();
+
+            var result = Sum(num2:55,num3:10,num1:5);
+            Console.WriteLine(result);
+
+            Show("A",str3:"C");
+
+            Show("salam");
+        }
+
+        static void ShowGroupNo()
+        {
+            Console.WriteLine("P231");
+        }
+
+        static void ShowStudentCount()
+        {
+            Console.WriteLine(17);
+            return;
+            Console.WriteLine(14);
+            Console.WriteLine(15);
+        }
+
+        static void ShowGroupInfo()
+        {
+            ShowGroupNo();
+            ShowStudentCount();
+        }
+        static void Show(string str1, string str2 = "_", string str3 = "_")
+        {
+            Console.WriteLine(str1 + str2 + str3);
+        }
+        static void Show(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static void Show(int num)
+        {
+            Console.WriteLine(num);
+        }
+
+        static void Show(char cr)
+        {
+            Console.WriteLine(cr);
+        }
+
+        static int Sum(int num1,int num2=100)
+        {
+            return num1 + num2;
+        }
+
+        static int Sum(int num1, int num2,int num3)
+        {
+            return num1 + num2+num3;
+        }
+
+        static double Sum(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+
+        
+
+
+
+    }
+}
